9a7f2f261653a7f4b6a9fd42fd4d05fe9e2d5754
Jack Chu <chuchuanbao@gmail.com>
2018-11-03 23:02:18 +0800

    docs(注释内容): 增加注释
    增加相关注释说明内容

--- src/main/java/com/godfather1103/app/RuleCheckApp.java
+++ src/main/java/com/godfather1103/app/RuleCheckApp.java
@@ -12,10 +12,26 @@ import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+/**
+ * <p>Title:        Godfather1103's Github</p>
+ * <p>Copyright:    Copyright (c) 2018</p>
+ * <p>Company:      https://github.com/godfather1103</p>
+ * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+ * 创建时间：2018/11/3 22:55
+ * @version 1.0
+ * @since
+ * 检测工具的入口
+ */
 public class RuleCheckApp {
 
-    ArrayList<RuleChecker> ruleCheckers = null;
+    ArrayList<RuleChecker> ruleCheckers;
 
+    /**
+     * 无参的构造函数（缺省加载内置的rule.json）<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:54
+     * @throws FailureException 相关构造函数的异常
+     */
     public RuleCheckApp() throws FailureException {
         try {
             String s = getStrFromSteam(RuleCheckApp.class.getResourceAsStream("/rule.json"),"UTF-8");
@@ -26,6 +42,15 @@ public class RuleCheckApp {
         }
     }
 
+    /**
+     * 读取输入流中的数据转成字符串返回<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:52
+     * @param  in 输入流
+     * @param  charset 字符串的编码方式
+     * @return 从输入流中读取到的字符串数据
+     * @throws IOException IO相关的异常
+     */
     public String getStrFromSteam(InputStream in,String charset) throws IOException {
         StringBuffer sb = new StringBuffer();
         byte [] bytes = new byte[1024];
@@ -36,6 +61,13 @@ public class RuleCheckApp {
         return sb.toString();
     }
 
+    /**
+     * 构造函数（加载传入的配置文件地址）<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:56
+     * @param  configFilePath 配置文件路径
+     * @throws FailureException 相关构造函数的异常
+     */
     public RuleCheckApp(String configFilePath) throws FailureException {
         try {
             String s =  FileUtils.readFileToString(new File(configFilePath),"UTF-8");
@@ -46,6 +78,14 @@ public class RuleCheckApp {
         }
     }
 
+    /**
+     * 检测方法<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:57
+     * @param  text 被检测的结果
+     * @return 无返回值
+     * @throws FailureException 相关检测的异常
+     */
     public void check(String text) throws FailureException {
         if (this.ruleCheckers==null){
             throw new FailureException("相关规则配置文件不存在！");
--- src/main/java/com/godfather1103/error/FailureException.java
+++ src/main/java/com/godfather1103/error/FailureException.java
@@ -9,12 +9,30 @@ package com.godfather1103.error;
  * 创建时间：2018/10/31 15:44
  * @version 1.0
  * @since
+ * 检测时根据情况向上抛出的异常类
  */
 public class FailureException extends Exception{
+
+    /**
+     * 无参的构造函数<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:58
+     * @param
+     * @return
+     * @throws
+     */
     public FailureException() {
         super("Commit Lint failed, please check rules");
     }
 
+    /**
+     * 带参数的构造函数<BR>
+     * @author  作者: godfa E-mail: chuchuanbao@gmail.com
+     * 创建时间：2018/11/3 22:59
+     * @param  message 异常的文字说明
+     * @return
+     * @throws
+     */
     public FailureException(String message) {
         super(message);
     }
